90b7d646c19751b01cb38b81f53b74dd1f1af41b
Alumno IDS <alumnoids@imac-de-sistemas.local>
2018-04-12 14:52:44 -0500

    Initial Commit

---
+++ Activity_12/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Activity_12
+//
+//  Created by Alumno IDS on 12/04/18.
+//  Copyright © 2018 Alumno IDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
